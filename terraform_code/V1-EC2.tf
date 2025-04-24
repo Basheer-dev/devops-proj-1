ecebb2ee8b464c995cce9fc16447baf353e081c3
Basheer <basheeruddin11@gmail.com>
2025-04-24 14:14:02 -0400

    adding v2 version

--- terraform_code/V1-EC2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
-resource "aws_instance" "demo-server" {
-  ami = "ami-0e449927258d45bc4"
-  instance_type = "t2.micro"
-  key_name = "jenkins-master"
-}
